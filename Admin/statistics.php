0bb9b2d3e7c6b1a27da604ecef8af3a962c86bec
leav1998 <luis.avila@unmsm.edu.pe>
2022-01-31 12:27:04 -0500

    traduciendo epicamente v4 con una grafica

--- Admin/statistics.php
+++ Admin/statistics.php
@@ -1,48 +1,87 @@
-<<!---------------- Session starts form here ----------------------->
-    <?php
-    session_start();
-    if (!$_SESSION["LoginAdmin"]) {
-        header('location:../login/login.php');
-    }
-    require_once "../connection/connection.php";
-    ?>
-    <!---------------- Session Ends form here ------------------------>
+<!---------------- Session starts form here ----------------------->
+<?php
+session_start();
+if (!$_SESSION["LoginAdmin"]) {
+    header('location:../login/login.php');
+}
+require_once "../connection/connection.php";
+?>
+<!---------------- Session Ends form here ------------------------>
+<?php
+$arrayDatos;
+$i = 0;
+$query = "Select course_code, COUNT(course_code) from student_info group by course_code";
+$run = mysqli_query($con, $query);
+while ($row = mysqli_fetch_array($run)) {
+    $arrayDatos[$i] = $row["COUNT(course_code)"];
+    $i++;
+}
+// echo $arrayDatos[1];
 
-    <!-- <?php
-            $message = "";
-            $account = "";
-            if (isset($_POST['submit'])) {
-                $account = $_POST['account'];
-                $user_id = $_POST['user_id'];
-                $que = "update login set account='$account' where user_id = '$user_id'";
-                $run = mysqli_query($con, $que);
-                if ($run) {
-                    $message =  $account == "Activate" ? "Account Activated Successfully" : "Account Deactivated Successfully";
-                } else {
-                    $message = "Account Not Activated  Successfully";
-                }
-            }
-            ?> -->
+?>
 
-    <!doctype html>
-    <html lang="es">
 
-    <head>
-        <title>FISI - Estadísticas</title>
-    </head>
+<!doctype html>
+<html lang="es">
 
-    <body>
-        <?php include('../common/common-header.php') ?>
-        <?php include('../common/admin-sidebar.php') ?>
+<head>
+    <title>FISI - Estadísticas</title>
+</head>
 
-        <main role="main" class="col-xl-10 col-lg-9 col-md-8 ml-sm-auto px-md-4 mb-2 w-100">
-            <div class="sub-main">
-                <div class="bar-margin text-center d-flex flex-wrap flex-md-nowrap pt-3 pb-2 mb-3 text-white admin-dashboard pl-3">
-                    <h4>Estadísticas </h4>
-                </div>
+<body>
+    <?php include('../common/common-header.php') ?>
+    <?php include('../common/admin-sidebar.php') ?>
 
+    <main role="main" class="col-xl-10 col-lg-9 col-md-8 ml-sm-auto px-md-4 mb-2 w-100">
+        <div class="sub-main">
+            <div class="bar-margin text-center d-flex flex-wrap flex-md-nowrap pt-3 pb-2 mb-3 text-white admin-dashboard pl-3">
+                <h4>Estadísticas </h4>
             </div>
-        </main>
-    </body>
+            <div class="col-sm-6">
+                <canvas id="myChart"></canvas>
+            </div>
 
-    </html>
+        </div>
+    </main>
+    <!-- Integración -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js" integrity="sha512-TW5s0IT/IppJtu76UbysrBH9Hy/5X41OTAbQuffZFU6lQ1rdcLHzpU5BzVvr/YFykoiMYZVWlr/PX1mDcfM9Qg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Configuración -->
+    <script>
+        const labels = [
+            'Ingeniería de Sistemas',
+            'Ingeniería de Software',
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+
+                data: [<?php
+                        echo $arrayDatos[0];
+                        ?>, <?php
+                            echo $arrayDatos[1];
+                            ?>],
+
+            }]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+    </script>
+    <!-- Inserción -->
+    <script>
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    </script>
+
+</body>
+
+</html>
