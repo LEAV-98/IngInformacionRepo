907d89b2faa18f523d82f006c0b37d117ea3f3e8
leav1998 <luis.avila@unmsm.edu.pe>
2022-01-31 21:35:57 -0500

    he vuelto despues que kgaron mi rama papus

--- Admin/statistics.php
+++ Admin/statistics.php
@@ -7,29 +7,25 @@ if (!$_SESSION["LoginAdmin"]) {
 require_once "../connection/connection.php";
 ?>
 <!---------------- Session Ends form here ------------------------>
+<?php
+$arrayDatos;
+$i = 0;
+$query = "Select course_code, COUNT(course_code) from student_info group by course_code";
+$run = mysqli_query($con, $query);
+while ($row = mysqli_fetch_array($run)) {
+    $arrayDatos[$i] = $row["COUNT(course_code)"];
+    $i++;
+}
+// echo $arrayDatos[1];
 
-<!-- <?php
-        $message = "";
-        $account = "";
-        if (isset($_POST['submit'])) {
-            $account = $_POST['account'];
-            $user_id = $_POST['user_id'];
-            $que = "update login set account='$account' where user_id = '$user_id'";
-            $run = mysqli_query($con, $que);
-            if ($run) {
-                $message =  $account == "Activate" ? "Account Activated Successfully" : "Account Deactivated Successfully";
-            } else {
-                $message = "Account Not Activated  Successfully";
-            }
-        }
-        ?> -->
+?>
+
 
 <!doctype html>
 <html lang="es">
 
 <head>
     <title>FISI - Estadísticas</title>
-    <link rel="shortcut icon" href="../Images/logo-unmsm.png" type="image/x-icon">
 </head>
 
 <body>
@@ -41,71 +37,51 @@ require_once "../connection/connection.php";
             <div class="bar-margin text-center d-flex flex-wrap flex-md-nowrap pt-3 pb-2 mb-3 text-white admin-dashboard pl-3">
                 <h4>Estadísticas </h4>
             </div>
+            <div class="col-sm-6">
+                <canvas id="myChart"></canvas>
+            </div>
 
-            ?>
+        </div>
+    </main>
+    <!-- Integración -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js" integrity="sha512-TW5s0IT/IppJtu76UbysrBH9Hy/5X41OTAbQuffZFU6lQ1rdcLHzpU5BzVvr/YFykoiMYZVWlr/PX1mDcfM9Qg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Configuración -->
+    <script>
+        const labels = [
+            'Ingeniería de Sistemas',
+            'Ingeniería de Software',
+        ];
 
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
 
-            <!doctype html>
-            <html lang="es">
+                data: [<?php
+                        echo $arrayDatos[0];
+                        ?>, <?php
+                            echo $arrayDatos[1];
+                            ?>],
 
-            <head>
-                <title>FISI - Estadísticas</title>
-            </head>
+            }]
+        };
 
-            <body>
-                <?php include('../common/common-header.php') ?>
-                <?php include('../common/admin-sidebar.php') ?>
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+    </script>
+    <!-- Inserción -->
+    <script>
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    </script>
 
-                <main role="main" class="col-xl-10 col-lg-9 col-md-8 ml-sm-auto px-md-4 mb-2 w-100">
-                    <div class="sub-main">
-                        <div class="bar-margin text-center d-flex flex-wrap flex-md-nowrap pt-3 pb-2 mb-3 text-white admin-dashboard pl-3">
-                            <h4>Estadísticas </h4>
-                        </div>
-                        <div class="col-sm-6">
-                            <canvas id="myChart"></canvas>
-                        </div>
+</body>
 
-                    </div>
-                </main>
-                <!-- Integración -->
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js" integrity="sha512-TW5s0IT/IppJtu76UbysrBH9Hy/5X41OTAbQuffZFU6lQ1rdcLHzpU5BzVvr/YFykoiMYZVWlr/PX1mDcfM9Qg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-                <!-- Configuración -->
-                <script>
-                    const labels = [
-                        'Ingeniería de Sistemas',
-                        'Ingeniería de Software',
-                    ];
-
-                    const data = {
-                        labels: labels,
-                        datasets: [{
-                            label: 'My First dataset',
-                            backgroundColor: 'rgb(255, 99, 132)',
-                            borderColor: 'rgb(255, 99, 132)',
-
-                            data: [<?php
-                                    echo $arrayDatos[0];
-                                    ?>, <?php
-                                        echo $arrayDatos[1];
-                                        ?>],
-
-                        }]
-                    };
-
-                    const config = {
-                        type: 'line',
-                        data: data,
-                        options: {}
-                    };
-                </script>
-                <!-- Inserción -->
-                <script>
-                    const myChart = new Chart(
-                        document.getElementById('myChart'),
-                        config
-                    );
-                </script>
-
-            </body>
-
-            </html>
+</html>
