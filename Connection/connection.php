93ceabb34790c4d922f0987f993db6a25072dc6b
Gabo29val <ga_al5@hotmail.com>
2022-02-01 11:22:10 -0500

    conexión bd local y remota

--- Connection/connection.php
+++ Connection/connection.php
@@ -1,9 +1,5 @@
 <?php 
-	$host = "bvkgjzsuuachv90vukle-mysql.services.clever-cloud.com";
-	$user = "uyup3tigpqverpeu";
-	$password = "1201RRyTYAj6wHDTatAe";
-	$name_database = "bvkgjzsuuachv90vukle";
-	$con=mysqli_connect($host,$user,$password,$name_database);
+	$con=mysqli_connect("localhost","root","","imperial_college");
 	if(!$con)
 	{
 		echo "Connection is not Successfully";
